b2c27a61939f87cd1cd73bb3c338b311a90a7bac
Aftab Alam <aftabislam786786@gmail.com>
2025-05-14 14:53:43 +0530

    correctness done

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let clearBtn = document.querySelector("clear");
- 
-function getTxt(){
-    let schoolNameTxt = document.getElementById("schoolName").value;
-    let studentNameTxt = document.getElementById("studentName").value;
-    let yearTxt = document.getElementById("year").value;
-    let rollNOTxt = document.getElementById("roll.no").value;
-    let contactNoTxt = document.getElementById("contact.no").value;
-
-    console.log(schoolNameTxt, studentNameTxt, yearTxt, rollNOTxt, contactNoTxt)
-}
 function doClear(){
     document.getElementById("schoolName").value = "";
     document.getElementById("studentName").value = "";
@@ -18,7 +7,7 @@ function doClear(){
 }
 function getIdCard(){
     document.getElementById("fill-form").classList.add("hide");
-    document.getElementById("idCard").style.display="inline";
+    document.getElementById("idCard").style.display="block";
     document.getElementById("idNav").innerText = document.getElementById("schoolName").value;
     document.getElementById("studentNameVal").innerText = document.getElementById("studentName").value;
     document.getElementById("yearVal").innerText = document.getElementById("year").value;
